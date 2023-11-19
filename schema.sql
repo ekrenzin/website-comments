65dcf2dcc5f978c33ae61ae49313e7f6e43aa64c
Ean Krenzin-Blank <ean@eankrenzin.com>
2023-11-19 00:40:57 -0800

    add code

---
+++ schema.sql
@@ -0,0 +1,17 @@
+-- Drop the Comments table if it already exists
+DROP TABLE IF EXISTS Comments;
+
+-- Create the Comments table
+CREATE TABLE IF NOT EXISTS Comments (
+    CommentID INTEGER PRIMARY KEY,  -- Unique identifier for each comment
+    User TEXT,                 -- Identifier for the user who made the comment
+    CommentText TEXT,               -- The actual comment
+    SourceID TEXT,               -- The ID of the source that the comment is about
+    CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,  -- Timestamp for when the comment was created
+    UpdatedAt DATETIME DEFAULT CURRENT_TIMESTAMP   -- Timestamp for when the comment was last updated
+);
+
+-- Sample INSERT statements to populate the Comments table
+INSERT INTO Comments (User, CommentText, SourceID) VALUES 
+('Ean', 'Welcome!', "welcome")
+
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,72 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	DB: D1Database;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	/**
+	 * Handles the fetch event
+	 * @param {Request} request - The incoming request
+	 * @param {Env} env - The environment containing the database connection
+	 * @returns {Promise<Response>} A response with the query results
+	 */
+	async fetch(request: Request, env: Env) {
+		const body = JSON.parse(await request.text());
+		
+		const { method } = body;
+		if (!method) {
+			// Handle the case where 'source' parameter is missing
+			return new Response('method parameter is required', { status: 400 });
+		}
+		if (method === 'list') {
+			return handleFetchComments(body, env);
+		} else if (method === 'upload') {
+			return handleUploadComment(body, env);
+		}
 	},
 };
+
+async function handleFetchComments(body: any, env: Env) {
+	const { source } = body;
+
+	if (!source) {
+		// Handle the case where 'source' parameter is missing
+		return new Response('source parameter is required', { status: 400 });
+	}
+
+	try {
+		// Prepare and execute the SQL query
+		const stmt = await env.DB.prepare("SELECT * FROM Comments WHERE SourceID = ?");
+		const results = await stmt.bind(source).all();
+
+		// Return the results in JSON format
+		return new Response(JSON.stringify(results), {
+			headers: { 'Content-Type': 'application/json' }
+		});
+	} catch (error) {
+		// Handle any errors that occur during the database query
+		return new Response(`Database error: ${error.message}`, { status: 500 });
+	}
+}
+
+async function handleUploadComment(body: any, env: Env) {
+	const { source, comment, user } = body;
+
+	if (!source || !comment || !user) {
+		// Handle the case where 'source' parameter is missing
+		return new Response('source user and comment parameters are required', { status: 400 });
+	}
+	
+	try {
+		// Prepare and execute the SQL query
+		const stmt = await env.DB.prepare("INSERT INTO Comments (SourceID, CommentText, User) VALUES (?, ?, ?)");
+		const results = await stmt.bind(source, comment, user).run();
+
+		// Return the results in JSON format
+		return new Response(JSON.stringify(results), {
+			headers: { 'Content-Type': 'application/json' }
+		});
+	} catch (error) {
+		// Handle any errors that occur during the database query
+		return new Response(`Database error: ${error.message}`, { status: 500 });
+	}
+}
